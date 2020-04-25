6fa479aa2962703ba6b3a578d1cf5bfe20f974bc
Philip Cali <philip.cali@gmail.com>
2020-04-25 14:07:49 -0400

    Adding inline content invalidation.

--- infrastructure/lib/infrastructure-stack.ts
+++ infrastructure/lib/infrastructure-stack.ts
@@ -2,7 +2,13 @@ import * as s3 from '@aws-cdk/aws-s3';
 import * as cloudfront from '@aws-cdk/aws-cloudfront';
 import * as acm from '@aws-cdk/aws-certificatemanager';
 import * as route53 from '@aws-cdk/aws-route53';
+import * as lambda from '@aws-cdk/aws-lambda';
+import * as events from '@aws-cdk/aws-lambda-event-sources';
+import * as iam from '@aws-cdk/aws-iam';
 import * as cdk from '@aws-cdk/core';
+
+import fs = require('fs');
+import path = require('path');
 
 const TTL = cdk.Duration.minutes(5);
 const DOMAIN_NAME = 'philcali.me';
@@ -16,6 +22,16 @@ export class InfrastructureStack extends cdk.Stack {
     const hostedZone = route53.HostedZone.fromHostedZoneAttributes(this, 'PersonalHZ', {
       hostedZoneId: ZONE_ID,
       zoneName: DOMAIN_NAME
+    });
+
+    const codePath = path.join(__dirname, 'handlers/invalidate.js');
+
+    const invalidation = new lambda.Function(this, 'Invalidation', {
+      handler: 'index.handler',
+      runtime: lambda.Runtime.NODEJS_10_X,
+      code: lambda.Code.fromInline(fs.readFileSync(codePath).toString()),
+      timeout: cdk.Duration.minutes(1),
+      memorySize: 128
     });
 
     const validatedCert = new acm.Certificate(this, 'AdventureWebsiteCert', {
@@ -52,6 +68,26 @@ export class InfrastructureStack extends cdk.Stack {
         behaviors: [{ isDefaultBehavior: true }]
       }]
     });
+
+    invalidation.addEnvironment('CDN', cdn.distributionId);
+    invalidation.addToRolePolicy(new iam.PolicyStatement({
+      effect: iam.Effect.ALLOW,
+      actions: [ 'cloudfront:createInvalidation' ],
+      resources: [ this.formatArn({
+        service: 'cloudfront',
+        region: '',
+        resource: `distribution/${cdn.distributionId}`,
+        sep: ':'
+      }) ]
+    }));
+
+
+    invalidation.addEventSource(new events.S3EventSource(websiteBucket, {
+      events: [ s3.EventType.OBJECT_CREATED ],
+      filters: [{
+        suffix: 'index.html'
+      }]
+    }));
 
     const adventureRecordSet = new route53.CnameRecord(this, 'AdventureCNAME', {
       domainName: cdn.domainName,
