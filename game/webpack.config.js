caee6f059ff9fdae90306e7e05488e84e719f155
Philip Cali <philip.cali@gmail.com>
2022-01-23 14:31:50 -0500

    chore: make lgtm happy

--- game/webpack.config.js
+++ game/webpack.config.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const CopyPlugin = require('copy-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
--- infrastructure/lib/infrastructure-stack.ts
+++ infrastructure/lib/infrastructure-stack.ts
@@ -3,7 +3,6 @@ import * as route53 from '@aws-cdk/aws-route53';
 import * as website from '@philcali-cdk/static-website';
 import * as cdk from '@aws-cdk/core';
 
-const TTL = cdk.Duration.minutes(5);
 const DOMAIN_NAME = 'philcali.me';
 const ZONE_ID = 'ZI7HL5YZ6FD32';
 
@@ -11,7 +10,6 @@ export class InfrastructureStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // TODO: import this resource into a template, pls
     const hostedZone = route53.HostedZone.fromHostedZoneAttributes(this, 'PersonalHZ', {
       hostedZoneId: ZONE_ID,
       zoneName: DOMAIN_NAME
@@ -22,7 +20,7 @@ export class InfrastructureStack extends cdk.Stack {
       validationMethod: acm.ValidationMethod.DNS
     });
 
-    const staticWebsite = new website.StaticWebsite(this, 'AdventureWebsite', {
+    new website.StaticWebsite(this, 'AdventureWebsite', {
       domainName: `adventure.${DOMAIN_NAME}`,
       certificate,
       hostedZone
